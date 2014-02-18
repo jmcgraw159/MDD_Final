f69ec201126d0d147260163927b2008a3dd20946
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-02-17 20:53:10 -0500

    Init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('mddFinalApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
