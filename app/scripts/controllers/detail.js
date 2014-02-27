b1cc77bb8de06beb38ee0e7607689103fcb11f0f
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-02-26 19:35:55 -0500

    Made it so non-logged in can't see the rest of the site

--- app/scripts/controllers/detail.js
+++ app/scripts/controllers/detail.js
@@ -1,31 +1,37 @@
 'use strict';
 
 var App = angular.module('mddFinalApp');
-App.controller('DetailCtrl', ['$scope', '$http', '$routeParams', '$sce', function ($scope, $http, $routeParams, $sce) {
+App.controller('DetailCtrl', ['$scope', '$http', '$routeParams', '$sce', '$rootScope', '$location', function ($scope, $http, $routeParams, $sce, $rootScope, $location) {
 
-    $http.jsonp('http://api.tumblr.com/v2/blog/wddcolumn.tumblr.com/posts/text?callback=JSON_CALLBACK&api_key=BhWySvI9jLXAwhFCYujxtqhAGw9e6BSOpxwiyKNkRduq1qMN7g&type=text&id=' + $routeParams.id)
-	.success(function(data){
-        $scope.article = data;
+      $rootScope.loginObject.$getCurrentUser()
+      .then(function(user){
+        if(!user){
+          $location.path('/');
+        }else {
+          $http.jsonp('http://api.tumblr.com/v2/blog/wddcolumn.tumblr.com/posts/text?callback=JSON_CALLBACK&api_key=BhWySvI9jLXAwhFCYujxtqhAGw9e6BSOpxwiyKNkRduq1qMN7g&type=text&id=' + $routeParams.id)
+          .success(function(data){
+                $scope.article = data;
 
-        $scope.image = 'http://api.tumblr.com/v2/blog/' + data.response.blog.name  + '.tumblr.com/avatar/64';
+                $scope.image = 'http://api.tumblr.com/v2/blog/' + data.response.blog.name  + '.tumblr.com/avatar/64';
 
-        $scope.snippet = data.response.posts[0].body;
+                $scope.snippet = data.response.posts[0].body;
 
-        $scope.trustSnippet = function() {
-            return $sce.trustAsHtml($scope.snippet);
-		      };
+                $scope.trustSnippet = function() {
+                    return $sce.trustAsHtml($scope.snippet);
+                  };
 
-		    $scope.date = Date.parse($scope.article.response.posts[0].date);
-      })
-	.error(function(data) {
-		  console.log(data);
-	  });
-
-  }]);
+                $scope.date = Date.parse($scope.article.response.posts[0].date);
+              })
+          .error(function(data) {
+              console.log(data);
+            });
+        }
+      });
+    }]);
 
 App.controller('CommentCtrl', ['$scope', '$rootScope', '$firebase', '$routeParams', function ($scope, $rootScope, $firebase, $routeParams) {
 
-    $scope.comments = $firebase(new Firebase("https://wddcolumn.firebaseio.com/comments/" + $routeParams.id));
+    $scope.comments = $firebase(new Firebase('https://wddcolumn.firebaseio.com/comments/' + $routeParams.id));
 
     $scope.commentDate = Date();
     $scope.parsedDate = Date.parse($scope.commentDate);
